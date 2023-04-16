24fe199a8c971308ef85673980ffcf9032a9c797
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-04-16 14:32:53 +0530

    Tab view complete

--- p16tabview/app/src/main/java/com/ajayam/p16tabview/MainActivity.java
+++ p16tabview/app/src/main/java/com/ajayam/p16tabview/MainActivity.java
@@ -1,14 +1,26 @@
 package com.ajayam.p16tabview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
 
+public class MainActivity extends AppCompatActivity {
+TabLayout tab;
+ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tab = findViewById(R.id.tab);
+        viewPager = findViewById(R.id.viewPager);
+
+        viewPagerMassangerAdapter adapter = new viewPagerMassangerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        tab.setupWithViewPager(viewPager);
     }
 }
---
+++ p16tabview/app/src/main/java/com/ajayam/p16tabview/viewPagerMassangerAdapter.java
@@ -0,0 +1,43 @@
+package com.ajayam.p16tabview;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class viewPagerMassangerAdapter extends FragmentPagerAdapter {
+    public viewPagerMassangerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        if (position==0) {
+            return new ChatFragment();
+        } else if (position==1) {
+            return new StatusFragment();
+        }
+        else {
+            return new CallsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3; //no. of tabs
+    }
+
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position==0){
+            return "Chats";
+        } else if (position==1) {
+            return "Status";
+        } else {
+            return "Calls";
+        }
+    }
+}
