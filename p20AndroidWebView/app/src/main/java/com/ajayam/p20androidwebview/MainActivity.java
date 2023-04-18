64f9efeeddc110609bfe42d1c87e1d7772a33ac3
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-04-18 17:18:40 +0530

    p20 Android WebView

---
+++ p20AndroidWebView/app/src/main/java/com/ajayam/p20androidwebview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.ajayam.p20androidwebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+     WebView webView;
+     ProgressBar pgBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        pgBar = findViewById(R.id.pgBar);
+
+        webView.loadUrl("https://ajayam.in");
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                pgBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                pgBar.setVisibility(View.GONE);
+                super.onPageFinished(view, url);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
